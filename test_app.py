0c2370f5761e2d560bf5527a2357e0ad0e645174
FaisalSaqallah <124791547+FaisalSaqallah@users.noreply.github.com>
2025-05-05 11:46:33 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,8 +1,8 @@
 import sys
-sys.path.append(".")
-from app import add_task
+sys.path.append('.')
+import app
 
 def test_add_task():
-    result = add_task("Test task")
+    result = app.add_task("Test task")
     assert result["task"] == "Test task"
     assert result["completed"] == False
